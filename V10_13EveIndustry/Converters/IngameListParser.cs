957bbcf2a5f8ec544278a1dfb4d917ae917f7155
Sleeper <Sleeper@WIN-9P5PRODUQ06>
2022-10-31 22:48:33 +0300

    Updates

---
+++ V10_13EveIndustry/Converters/IngameListParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using V10_13EveIndustry.Units;
+
+namespace V10_13EveIndustry.Converters
+{
+    public static class Parser
+    {
+        public static List<string> Parse(string data, char Devider = '\t')
+        {
+            List<string> list = new List<string>();
+            while(data.IndexOf(Devider) > -1)
+            {
+                list.Add(data.Substring(0, data.IndexOf(Devider)));
+                data = data.Substring(data.IndexOf(Devider) + 1);
+            }
+            return list;
+        }
+    }
+}
+
+/*
+ID Названия Количество	Оценка стоимости 
+1	Гражданского образца Рельсотрон с нарезным стволом Small	1	1500.0 
+2	MK7 Рельсотрон Medium с нарезным стволом	2	63552.66 
+3	Federation Navy Короткоствольный рельсотрон Small	1	289545.0 
+
+ID	Названия	Количество	Оценка стоимости 
+1	Гражданского образца Рельсотрон с нарезным стволом Small	1	1500.0 
+2	MK7 Рельсотрон Medium с нарезным стволом	1	31776.33 
+3	Federation Navy Короткоствольный рельсотрон Small	1	289545.0 
+
+*/
---
+++ V10_13EveIndustry/Units/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace V10_13EveIndustry.Units
+{
+    public class Item
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("count")]
+        public int Count { get; set; }
+
+        [JsonProperty("cost")]
+        public int Cost { get; set; }
+
+        public Item(string name)
+        {
+            Name = name;
+            Count = 1;
+            Cost = 0;
+        }
+        public static Item FromRowData(string d)
+        {
+            
+        }
+    }
+}
